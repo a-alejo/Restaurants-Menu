6881404f2ef7424053ed106d3a5db19b18aaf986
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-01-24 13:53:27 -0500

    Update app.py
    01-24-21 Cleaned the code a bit

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from Basic_Meals import Basic_Meals
 from Chinese_Meals import Chinese_Meals
 from Italian_Meals import Italian_Meals
 
-
-
 myMeals = Basic_Meals()
 myMeals.appetizer()
 
@@ -12,5 +10,3 @@ myChinese.appetizer()
 
 myItalian = Italian_Meals()
 myItalian.appetizer()
-
-
