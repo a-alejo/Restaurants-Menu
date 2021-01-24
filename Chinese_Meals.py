c0392e1a537b22d4e72a60b9eb9c9d9101c81a24
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-01-24 13:54:14 -0500

    Update Chinese_Meals.py
    01-24-21 Cleaned the code a bit

--- Chinese_Meals.py
+++ Chinese_Meals.py
@@ -1,9 +1,12 @@
 from Basic_Meals import Basic_Meals
 
+
 class Chinese_Meals(Basic_Meals):
     def chicken_fried_rice(self):
         print('Chicken fried rice is on the menu')
+
     def chow_mein(self):
         print('Chow mein is on the menu')
+
     def appetizer(self):
         print('Spring rolls is on the menu')
